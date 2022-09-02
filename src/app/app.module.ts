1b6007538f7f04118fbd01af3aad1d9cd903e73e
Manuel Pereira <manilex2@gmail.com>
2022-09-02 12:27:16 -0500

    Arregladas las fechas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
 import { StoreModule } from '@ngrx/store';
 import { ToastrModule } from 'ngx-toastr';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 /************** COMPONENTES ********************/
 import { AppComponent } from './app.component';
@@ -17,11 +18,12 @@ import { NavComponent } from './home/components/controllers/nav.component';
 /**************** COMMONS **********************/
 import myLocaleES from '@angular/common/locales/es-EC';
 import { registerLocaleData } from "@angular/common";
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
 
-/***************** EFFECTS **********************/
+/***************** EFFECTS ********************/
+import { EffectsModule } from '@ngrx/effects';
+
+/***************** ENVIRONMENTS ********************/
+import { environment } from '../environments/environment';
 
 registerLocaleData(myLocaleES);
 
